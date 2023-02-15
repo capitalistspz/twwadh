c1d188db689da159ebf4932afe0402e17e0e5e18
capitalistspz <keipitalists@proton.me>
2023-02-15 00:28:29 +0000

    Changes for readability

--- src/main/java/capitalistspz/twwadh/mixin/server/command/RideCommandMxn.java
+++ src/main/java/capitalistspz/twwadh/mixin/server/command/RideCommandMxn.java
@@ -4,7 +4,6 @@ import capitalistspz.twwadh.interfaces.IForcedMount;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -31,15 +30,14 @@ public abstract class RideCommandMxn {
             value="INVOKE",
             target="Lcom/mojang/brigadier/CommandDispatcher;register(Lcom/mojang/brigadier/builder/LiteralArgumentBuilder;)Lcom/mojang/brigadier/tree/LiteralCommandNode;")
             )
-    private static<S> LiteralArgumentBuilder<S> addForcedArgument(LiteralArgumentBuilder<S> command){
-        //noinspection unchecked
-        return command.then((ArgumentBuilder<S, ?>)(argument("target", EntityArgumentType.entity())
-                .then(literal("mount")
-                    .then(argument("vehicle", EntityArgumentType.entity())
-                            .then(argument("forced", BoolArgumentType.bool())
-                                    .executes(cmd -> executeMountForced(cmd.getSource(), EntityArgumentType.getEntity(cmd, "target"), EntityArgumentType.getEntity(cmd, "vehicle"), BoolArgumentType.getBool(cmd, "forced"))))))));
+    private static<S> LiteralArgumentBuilder<S> addForcedArgument(LiteralArgumentBuilder<S> command) {
+        var lab = argument("target", EntityArgumentType.entity()).then(
+                literal("mount").then(
+                        argument("vehicle", EntityArgumentType.entity()).then(
+                                argument("forced", BoolArgumentType.bool())
+                                        .executes(cmd -> executeMountForced(cmd.getSource(), EntityArgumentType.getEntity(cmd, "target"), EntityArgumentType.getEntity(cmd, "vehicle"), BoolArgumentType.getBool(cmd, "forced"))))));
+        return command.then((ArgumentBuilder<S, ?>) lab);
     }
-
     private static int executeMountForced(ServerCommandSource src, Entity rider, Entity vehicle, boolean forced) {
         int result = executeMount(src, rider, vehicle);
         if (forced && (rider instanceof PlayerEntity player)){
