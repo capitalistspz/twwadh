f3ffea4e5502435d9a0ea9605979f48cbb839f89
capitalistspz <keipitalists@proton.me>
2023-02-18 16:39:48 +0000

    Remade and fixed AxisArgumentType

---
+++ src/main/java/capitalistspz/twwadh/mixin/command/arguments/ArgumentTypesMxn.java
@@ -0,0 +1,26 @@
+package capitalistspz.twwadh.mixin.command.arguments;
+
+import capitalistspz.twwadh.command.AxisArgumentType;
+import com.mojang.brigadier.arguments.ArgumentType;
+import net.minecraft.command.argument.ArgumentTypes;
+import net.minecraft.command.argument.serialize.ArgumentSerializer;
+import net.minecraft.command.argument.serialize.ConstantArgumentSerializer;
+import net.minecraft.registry.Registry;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ArgumentTypes.class)
+public class ArgumentTypesMxn {
+
+    @Shadow
+    private static <A extends ArgumentType<?>, T extends ArgumentSerializer.ArgumentTypeProperties<A>> ArgumentSerializer<A, T> register(Registry<ArgumentSerializer<?, ?>> registry, String id, Class<? extends A> clazz, ArgumentSerializer<A, T> serializer){
+        return null;
+    }
+    @Inject(at=@At("HEAD"), method = "register(Lnet/minecraft/registry/Registry;)Lnet/minecraft/command/argument/serialize/ArgumentSerializer;")
+    private static void registerCustomArgumentTypes(Registry<ArgumentSerializer<?, ?>> registry, CallbackInfoReturnable<ArgumentSerializer<?, ?>> cir){
+        register(registry, "axis", AxisArgumentType.class, ConstantArgumentSerializer.of(AxisArgumentType::axis));
+    }
+}
